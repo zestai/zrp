9a6d1aa3eabca3c2f73a3a4aba2adf046c67448e
Christien Williams <christien.williams@gmail.com>
2022-02-07 09:59:13 -0500

    Updates download script

--- zrp/about.py
+++ zrp/about.py
@@ -1,5 +1,5 @@
 __title__ = "zrp"
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __download_url_prefix__ = "https://github.com/zestai/zrp/archive/refs/tags/"
 __lookup_table_download_url_prefix__ = "https://github.com/zestai/zrp/releases/download/"
 __projects__ = "https://github.com/zestai"
