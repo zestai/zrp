722182a5c81c41be29b41347313ca217ea35360a
kam <kam@zest.ai>
2022-02-09 23:47:51 +0000

    remove unused bisg reference

--- zrp/prepare/__init__.py
+++ zrp/prepare/__init__.py
@@ -5,7 +5,6 @@ from .geo_geocoder import ZGeo
 # from .geo_lookup import 
 from .preprocessing import  ProcessStrings, ProcessGeo, ProcessACS
 from .prepare import ZRP_Prepare
-from .generate_bisg import BISGWrapper
 
 
-__all__ = ['BaseZRP','ZRP_Predict','ZRP_Prepare', 'ProcessStrings', 'ProcessGeo', 'ProcessACS', 'ACSModelPrep', 'BISGWrapper' ]
+__all__ = ['BaseZRP','ZRP_Predict','ZRP_Prepare', 'ProcessStrings', 'ProcessGeo', 'ProcessACS', 'ACSModelPrep' ]
