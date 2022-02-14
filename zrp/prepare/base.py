0ac371de45281fb81c677cdc0d1a61bdae78ae25
chriswill21 <christien.williams@gmail.com>
2022-02-14 14:47:02 -0500

    Update base.py
    Update docstrings

--- zrp/prepare/base.py
+++ zrp/prepare/base.py
@@ -9,53 +9,55 @@ class BaseZRP():
     
     Parameters
     ----------
-    data: dataframe
-        dataframe with user data
-
-    state_mapping: dictionary
-        dictionary mapping state names & abbreviations
-    key: str 
+    support_files_path: "data/processed"
+        File path with support data.
+    key: str, default 'ZEST_KEY'
         Key to set as index. If not provided, a key will be generated.
-        
-    first_name: str
+    first_name: str, default 'first_name'
         Name of first name column
-        
-    middle_name: str
+    middle_name: str, default 'middle_name'
         Name of middle name column
-    last_name: str
+    last_name: str, default 'last_name'
         Name of last name/surname column
-    house_number: str
+    house_number: str, default 'house_number'
         Name of house number column. Also known as primary address number this is the unique number assigned to a building to delineate it from others on a street. This is usually the first component of a delivery address line.
-    street_address: str
+    street_address: str, default 'street_address'
         Name of street address column. The street address is usually comprised of predirectional, street name, and street suffix. 
-    city: str
-        Name of city column
-    state: str
-        Name of state column
-    zip_code: str
-        Name of zip or postal code column
+    city: str, default 'city'
+        Name of city column.
+    state: str, default 'state'
+        Name of state column.
+    zip_code: str, default 'zip_code'
+        Name of zip or postal code column.
+    race: str, default 'race'
+        Name of the race column
+    proxy: str, default 'probs'
+        Specifies whether to return proxy results as 'probs' (the proxy probabilities) or 'labels'.
     census_tract: str
         Name of census tract column
-    support_files_path:
-        File path with support data
     street_address_2: str, optional
         Name of additional address column
     name_prefix: str, optional
         Name of column containing full name preix (ie Dr, Sr, and Esq )
     name_suffix: str, optional
         Name of column containing full name suffix (ie jr, iii, and phd)
-    na_values: list
-        List of missing values to replace 
-    file_path: str
-        Input data file path
-    geocode: bool
-        Whether to geocode
-    bisg=True
-        Whether to return BISG proxies
-    readout: bool
-        Whether to return a readout
-    n_jobs: int (default 1)
+    na_values: list, optional
+        List of missing values to replace.
+    file_path: str, optional
+        Path where to put artifacts and other files generated during intermediate steps. 
+    geocode: bool, default True
+        Whether to geocode.
+    bisg: bool, default True
+        Whether to return BISG proxies. 
+    readout: bool, default True
+        Whether to return a readout.
+    n_jobs: int, default 49
         Number of jobs in parallel
+    year: str, default '2019'
+        ACS year to use.
+    span: str, default '5'
+        Year span of ACS data to use.
+    runname: str, default 'test'
     """
 
     def __init__(self, support_files_path="data/processed", key="ZEST_KEY", first_name="first_name",
