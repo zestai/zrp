0a6b02b206703c68ee1acd617becb80586d61b67
Christien Williams <christienwilliams@Christiens-MacBook-Pro-2.local>
2022-04-11 17:03:24 -0400

    Updates setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
-README = open((HERE / "README.md"))
+README = open((HERE / "README.md")).read_text()
 
 # The contents of the requirements.txt
 with open("requirements.txt", 'r') as file:
@@ -18,7 +18,7 @@ for r in requirements_raw:
 # This call to setup() does all the work
 setup(
     name="zrp",
-    version="0.2.1",
+    version="0.2.2",
     description="The Zest Race Predictor tool predicts race/ethnicity using a name and address as inputs.",
     long_description=README,
     long_description_content_type="text/markdown",
